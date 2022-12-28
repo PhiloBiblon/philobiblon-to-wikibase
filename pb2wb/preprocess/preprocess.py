f0bf7407bb89a8d02ef880e2058ee34a4a9958a4
Max Ziff <max.ziff@concur.com>
2022-12-28 10:27:37 -0800

    Fix the institutions preprocessor
    I added a new dependency: csvkit. I use it to clean csvs (dataclips)
    that contain a newline in a quoted field. Run clean/mkclean.sh.
    
    I updated paths. Note that the code now uses the downloaded, cleaned
    dataclip csv - not the one that had been checked in.

--- pb2wb/preprocess/preprocess.py
+++ pb2wb/preprocess/preprocess.py
@@ -13,22 +13,22 @@ def preprocess(table):
   os.makedirs(beta_pre_processed_dir, exist_ok=False)
 
   if table is None or table is Table.ANALYTIC:
-    GenericPreprocessor().preprocess(get_full_input_path('BETA/beta_analytic.csv'), beta_pre_processed_dir)
+    GenericPreprocessor().preprocess(get_full_input_path('BETA/csvs/beta_analytic.csv'), beta_pre_processed_dir)
   if table is None or table is Table.BIBLIOGRAPHY:
-    GenericPreprocessor().preprocess(get_full_input_path('BETA/beta_bibliography.csv'), beta_pre_processed_dir)
+    GenericPreprocessor().preprocess(get_full_input_path('BETA/csvs/beta_bibliography.csv'), beta_pre_processed_dir)
   if table is None or table is Table.BIOGRAPHY:
-    BiographyPreprocessor().preprocess(get_full_input_path('BETA/beta_biography.csv'), get_full_input_path('BETA/beta_geography.csv'), beta_pre_processed_dir)
+    BiographyPreprocessor().preprocess(get_full_input_path('BETA/csvs/beta_biography.csv'), get_full_input_path('BETA/csvs/beta_geography.csv'), beta_pre_processed_dir)
   if table is None or table is Table.COPIES:
-    GenericPreprocessor().preprocess(get_full_input_path('BETA/beta_copies.csv'), beta_pre_processed_dir)
+    GenericPreprocessor().preprocess(get_full_input_path('BETA/csvs/beta_copies.csv'), beta_pre_processed_dir)
   if table is None or table is Table.GEOGRAPHY:
-    GenericPreprocessor().preprocess(get_full_input_path('BETA/beta_geography.csv'), beta_pre_processed_dir)
+    GenericPreprocessor().preprocess(get_full_input_path('BETA/csvs/beta_geography.csv'), beta_pre_processed_dir)
   if table is None or table is Table.INSTITUTIONS:
-    InstitutionPreprocessor().preprocess(get_full_input_path('BETA/beta_institutions.csv'), beta_pre_processed_dir)
+    InstitutionPreprocessor().preprocess(get_full_input_path('BETA/csvs/beta_institutions.csv'), beta_pre_processed_dir)
   if table is None or table is Table.LIBRARY:
-    GenericPreprocessor().preprocess(get_full_input_path('BETA/beta_library.csv'), beta_pre_processed_dir)
+    GenericPreprocessor().preprocess(get_full_input_path('BETA/csvs/beta_library.csv'), beta_pre_processed_dir)
   if table is None or table is Table.MS_ED:
-    GenericPreprocessor().preprocess(get_full_input_path('BETA/beta_ms_ed.csv'), beta_pre_processed_dir)
+    GenericPreprocessor().preprocess(get_full_input_path('BETA/csvs/beta_ms_ed.csv'), beta_pre_processed_dir)
   if table is None or table is Table.SUBJECT:
-    GenericPreprocessor().preprocess(get_full_input_path('BETA/beta_subject.csv'), beta_pre_processed_dir)
+    GenericPreprocessor().preprocess(get_full_input_path('BETA/csvs/beta_subject.csv'), beta_pre_processed_dir)
   if table is None or table is Table.UNIFORM_TITLE:
-    GenericPreprocessor().preprocess(get_full_input_path('BETA/beta_uniform_title.csv'), beta_pre_processed_dir)
+    GenericPreprocessor().preprocess(get_full_input_path('BETA/csvs/beta_uniform_title.csv'), beta_pre_processed_dir)
