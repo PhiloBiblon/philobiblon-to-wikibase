a73bfb7a801eeaa043f1ef5e015ab52786ba81cb
Max Ziff <max.ziff@concur.com>
2023-03-01 10:24:47 -0800

    clean all the things

--- pb2wb/clean/mkclean.sh
+++ pb2wb/clean/mkclean.sh
@@ -3,70 +3,22 @@
 # Note: the schema for the dataclips may change when we get the next drop from John
 
 pbbase=`git rev-parse --show-toplevel`
+cd $pbbase/pb2wb
+
+for BIB in BETA BITECA BITAGAP
+do
+    bash clean/mkclean_bib.sh $BIB
+    echo
+done
+
 cd $pbbase/data
 
-# At present, this script works only with BETA and the Data Dictionary (same for all bibliographies)
-# TODO: add the other bibliographies; blocked by pbbase/data/fetch_from_gdrive and
-# pbbase/data/google_ids.csv. Also: we need to change the directory hierarchy in data/clean
-
-# Clean BETA table csvs
-
-# The raw_links directory is used as a staging area and a place in which to rename the individual files
-mkdir -p ./clean/raw_links
-
-ln -f './raw/BETA/csvs/BETA - ANALYTIC'* ./clean/raw_links/beta_analytic.csv
-ln -f './raw/BETA/csvs/BETA - BIBLIOGRAPHY'* ./clean/raw_links/beta_bibliography.csv
-ln -f './raw/BETA/csvs/BETA - BIOGRAPHY'* ./clean/raw_links/beta_biography.csv
-ln -f './raw/BETA/csvs/BETA - COPIES'* ./clean/raw_links/beta_copies.csv
-ln -f './raw/BETA/csvs/BETA - GEOGRAPHY'* ./clean/raw_links/beta_geography.csv
-ln -f './raw/BETA/csvs/BETA - INSTITUTIONS'* ./clean/raw_links/beta_institutions.csv
-ln -f './raw/BETA/csvs/BETA - LIBRARY'* ./clean/raw_links/beta_library.csv
-ln -f './raw/BETA/csvs/BETA - MS_ED'* ./clean/raw_links/beta_ms_ed.csv
-ln -f './raw/BETA/csvs/BETA - SUBJECT'* ./clean/raw_links/beta_subject.csv
-ln -f './raw/BETA/csvs/BETA - UNIFORM_TITLE'* ./clean/raw_links/beta_uniform_title.csv
-
-mkdir -p ./clean/BETA/csvs
-
-for f in `ls ./clean/raw_links`
-do
-    echo 'checking for illegal utf8 in: ' $f
-    cat $pbbase/data/clean/raw_links/$f | bash $pbbase/pbcsv/utf8check.sh
-    cat $pbbase/data/clean/raw_links/$f | bash $pbbase/pbcsv/utf8clean.sh > $pbbase/data/clean/BETA/csvs/$f
-done
-
-# Now we can throw away the raw_links directory
-
-rm -rf ./clean/raw_links
-
-# For compatibility (BETA only) we will also link clean csvs into $pbbase/data/clean/BETA
-
-### ln -f $pbbase/data/clean/BETA/csvs/*.csv $pbbase/data/clean/BETA
-
-# Clean the BETA dataclips
-
-# Bring back the raw_links directory
-mkdir -p ./clean/raw_links
-
-ln -f './raw/BETA/dataclips/BETA - DATACLIPS'* ./clean/raw_links/beta_dataclips.csv
-
-mkdir -p ./clean/BETA/dataclips
-
-for f in `ls ./clean/raw_links`
-do
-    echo 'checking for illegal utf8 in BETA dataclips: ' $f
-    cat $pbbase/data/clean/raw_links/$f | bash $pbbase/pbcsv/utf8check.sh
-    cat $pbbase/data/clean/raw_links/$f | bash $pbbase/pbcsv/utf8clean.sh | \
-        csvformat -M '@' | tr '\n@' ' \n' | sed 1d | \
-        (echo "code,class,status,census,es,en,unk1,unk2"; cat -) > $pbbase/data/clean/BETA/dataclips/$f
-done
-
-# Now we can throw away the raw_links directory
-
-rm -rf ./clean/raw_links
+echo
+echo Cleaning the datadict
 
 # Clean the datadict
 
-# Bring back the raw_links directory
+# Create a raw_links directory
 mkdir -p ./clean/raw_links
 
 ln -f ./raw/datadict/*ANA* ./clean/raw_links/analytic_datadict.csv
