2e88dacde50c598bbb522bcc4e0e32f6ccb9c538
Max Ziff <ziff@altiscale.com>
2024-04-28 14:30:52 -0700

    support other bibs, small samples for experimentation, skip existing

--- pb2wb/base_import/base_import.py
+++ pb2wb/base_import/base_import.py
@@ -1,44 +1,35 @@
 import os
-import base_import.mapper.moniker as mapper
-from common.wb_manager import WBManager
+
+from base_import.mapper.moniker import (AnalyticMonikerMapper, BibliographyMonikerMapper, BiographyMonikerMapper,
+                                        CopiesMonikerMapper, GeographyMonikerMapper, InstitutionMonikerMapper,
+                                        LibraryMonikerMapper, MsEdMonikerMapper, SubjectMonikerMapper,
+                                        UniformTitleMonikerMapper)
 from common.settings import CLEAN_DIR
+from common.wb_manager import WBManager
 
 
-def get_full_input_path(file):
-  return os.path.join(CLEAN_DIR, file)
+def get_full_input_path(bib, table):
+    file = f'{bib}/csvs/{bib.lower()}_{table.value.lower()}.csv'
+    return os.path.join(CLEAN_DIR, file)
 
-def base_import():
-  print('Preparing wikibase connection ...')
-  wb_manager = WBManager()
 
-  print('Migrating analytic ...')
-  mapper.AnalyticMonikerMapper(wb_manager).migrate(get_full_input_path('BETA/csvs/beta_analytic.csv'))
+def base_import(bib='BETA', table=None, skip_existing=False, dry_run=False, sample_size=0):
+    print('Preparing wikibase connection ...')
+    if dry_run:
+        wb_manager = None
+    else:
+        wb_manager = WBManager()
 
-  print('Migrating bibliography ...')
-  mapper.BibliographyMonikerMapper(wb_manager).migrate(get_full_input_path('BETA/csvs/beta_bibliography.csv'))
+    for mapper_class in [AnalyticMonikerMapper, BibliographyMonikerMapper, BiographyMonikerMapper, CopiesMonikerMapper,
+              GeographyMonikerMapper, InstitutionMonikerMapper, LibraryMonikerMapper, MsEdMonikerMapper,
+              SubjectMonikerMapper, UniformTitleMonikerMapper]:
+        if table is None or table is mapper_class.TABLE:
+            path = get_full_input_path(bib, mapper_class.TABLE)
+            print(f'Migrating {mapper_class.TABLE} from input {path} ...')
+            mapper = (mapper_class(wb_manager).
+                      with_sample_size(sample_size).
+                      with_dry_run(dry_run).
+                      with_skip_existing(skip_existing))
+            mapper.migrate( path)
 
-  print('Migrating biography ...')
-  mapper.BiographyMonikerMapper(wb_manager).migrate(get_full_input_path('BETA/csvs/beta_biography.csv'))
-
-  print('Migrating copies ...')
-  mapper.CopiesMonikerMapper(wb_manager).migrate(get_full_input_path('BETA/csvs/beta_copies.csv'))
-
-  print('Migrating geography ...')
-  mapper.GeographyMonikerMapper(wb_manager).migrate(get_full_input_path('BETA/csvs/beta_geography.csv'))
-
-  print('Migrating institution ...')
-  mapper.InstitutionMonikerMapper(wb_manager).migrate(get_full_input_path('BETA/csvs/beta_institutions.csv'))
-
-  print('Migrating library ...')
-  mapper.LibraryMonikerMapper(wb_manager).migrate(get_full_input_path('BETA/csvs/beta_library.csv'))
-
-  print('Migrating ms/ed ...')
-  mapper.MsEdMonikerMapper(wb_manager).migrate(get_full_input_path('BETA/csvs/beta_ms_ed.csv'))
-
-  print('Migrating subject ...')
-  mapper.SubjectMonikerMapper(wb_manager).migrate(get_full_input_path('BETA/csvs/beta_subject.csv'))
-
-  print('Migrating uniform title ...')
-  mapper.UniformTitleMonikerMapper(wb_manager).migrate(get_full_input_path('BETA/csvs/beta_uniform_title.csv'))
-
-  print('done.')
+    print('done.')
--- pb2wb/run_base_import.py
+++ pb2wb/run_base_import.py
@@ -1,3 +1,24 @@
 from base_import import base_import
 
-base_import.base_import()
+import argparse
+
+from common import enums
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--bib', type=str, default="BETA",
+                    help="Top-level bib to preprocess. Default: 'BETA'")
+parser.add_argument('--skip_existing', action='store_true',
+                    help='Skip any objects that already exist')
+parser.add_argument('--dry_run', action='store_true',
+                    help='Dry run: do not side-effect the wikibase')
+parser.add_argument('--sample_size', type=int, default=0,
+                    help='Specify the sample size as a positive integer (default: 0)')
+parser.add_argument("--table", help="Table to process. Default is process all")
+args = parser.parse_args()
+
+if args.table:
+  args.table = enums.Table[args.table.upper()]
+
+base_import.base_import(bib=args.bib, table=args.table,
+                        skip_existing=args.skip_existing, dry_run=args.dry_run,
+                        sample_size=args.sample_size)
