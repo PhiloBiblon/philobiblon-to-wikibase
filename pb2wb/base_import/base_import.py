d6a4a55352e26d53ecfc484642ce18fe6c723a30
Jason Hatfield <jhatfi@gmail.com>
2024-07-24 11:50:17 -0700

    updated to base_import

--- pb2wb/base_import/base_import.py
+++ pb2wb/base_import/base_import.py
@@ -8,12 +8,15 @@ from common.settings import CLEAN_DIR
 from common.wb_manager import WBManager
 
 
-def get_full_input_path(bib, table):
+def get_full_input_path(bib, table, updated):
+    if updated:
+        file = f'updated/{bib.lower()}/{bib.lower()}_{table.value.lower()}.csv'
+        return file
     file = f'{bib}/csvs/{bib.lower()}_{table.value.lower()}.csv'
     return os.path.join(CLEAN_DIR, file)
 
 
-def base_import(bib='BETA', table=None, skip_existing=False, dry_run=False, sample_size=0):
+def base_import(bib='BETA', table=None, skip_existing=False, dry_run=False, sample_size=0, updated=False):
     print('Preparing wikibase connection ...')
     if dry_run:
         wb_manager = None
@@ -24,7 +27,7 @@ def base_import(bib='BETA', table=None, skip_existing=False, dry_run=False, samp
               GeographyMonikerMapper, InstitutionMonikerMapper, LibraryMonikerMapper, MsEdMonikerMapper,
               SubjectMonikerMapper, UniformTitleMonikerMapper]:
         if table is None or table is mapper_class.TABLE:
-            path = get_full_input_path(bib, mapper_class.TABLE)
+            path = get_full_input_path(bib, mapper_class.TABLE, updated)
             print(f'Migrating {mapper_class.TABLE} from input {path} ...')
             mapper = (mapper_class(wb_manager).
                       with_sample_size(sample_size).
