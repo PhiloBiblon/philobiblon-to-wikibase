e86ee7f2db152e13f53ff2a1d2de9e9715ab38f7
Jason Hatfield <jhatfi@gmail.com>
2024-07-26 12:06:26 -0700

    update for spot load

--- pb2wb/run_base_import.py
+++ pb2wb/run_base_import.py
@@ -16,6 +16,7 @@ parser.add_argument('--sample_size', type=int, default=0,
 parser.add_argument("--table", help="Table to process. Default is process all")
 parser.add_argument('--updated', action='store_true', help="Process missing objects. Default is false")
 parser.add_argument('--wb', default="PBSANDBOX", help="Select wiki base to use. Default is PBSANDBOX")
+parser.add_argument('--spot', action='store_true', help="Perform spot test on 10 records from each table")
 args = parser.parse_args()
 
 if args.table:
@@ -24,4 +25,5 @@ if args.table:
 base_import.base_import(bib=args.bib, table=args.table,
                         skip_existing=args.skip_existing, dry_run=args.dry_run,
                         sample_size=args.sample_size, updated=args.updated,
-                        wb=args.wb)
+                        wb=args.wb, spot=args.spot)
+ 
