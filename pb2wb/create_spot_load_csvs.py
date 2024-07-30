6141cdda0e963787272b159828a48c924fb68ed1
Jason Hatfield <jhatfi@gmail.com>
2024-07-30 10:37:33 -0700

    updated to grab all rows from first 10 id's

--- pb2wb/create_spot_load_csvs.py
+++ pb2wb/create_spot_load_csvs.py
@@ -28,15 +28,22 @@ files = [f'{bibliography}_analytic.csv', f'{bibliography}_biography.csv', f'{bib
 
 for file in files:
     print(f'Processing file: {file}')
-    input_file = f'../data/clean/{bibliography.lower()}/csvs/{file}'
+    input_file = f'../data/clean/{bibliography.upper()}/csvs/{file}'
     output_file = f'{updated_path}/{file}'
-    print(f'Using input file: {file}')
-    pre_df = pd.read_csv(f'../data/processed/pre/{bibliography.upper()}/{file}')
+    print(f'Using input file: {input_file}')
+    pre_df = pd.read_csv(input_file)
   
-    # Select the first 10 rows from the pre_df dataframe
-    first_10_rows = pre_df.head(10)
+    # Select the first column from the pre_df dataframe
+    first_column = pre_df.iloc[:, 0]
 
-    # Write the first 10 rows to a new CSV file
-    first_10_rows.to_csv(output_file, index=False)
+    # Get the unique values from the first column
+    unique_ids = first_column.unique()
+
+    # Select the first 10 unique ids
+    selected_ids = unique_ids[:10]
+
+    # Filter the pre_df dataframe based on the selected ids and write to a new csv file
+    filtered_df = pre_df[pre_df.iloc[:, 0].isin(selected_ids)]
+    filtered_df.to_csv(output_file, index=False)
 
 print(f'Complete. See the updated files in the spot_test/{bibliography} directory.')
