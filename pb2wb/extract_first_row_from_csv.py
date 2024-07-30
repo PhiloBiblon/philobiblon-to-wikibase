5c53a39d82e642f7a6ebccdc79dcb943680f541b
Jason Hatfield <jhatfi@gmail.com>
2024-07-30 12:38:52 -0700

    updated first row script

--- pb2wb/extract_first_row_from_csv.py
+++ pb2wb/extract_first_row_from_csv.py
@@ -1,23 +1,55 @@
 import pandas as pd
 import sys
+import os
+import argparse
+import datetime
+from pathlib import Path
 
-# Check if the CSV file path is provided as a command line argument
-if len(sys.argv) < 3:
-    print("Please provide the CSV input/output file path(s) as a command line argument.")
-    sys.exit(1)
+# Get the current date in YYYYMMDD format
+date = datetime.datetime.now().strftime("%Y%m%d")
 
-# Get the CSV file path from the command line argument
-csv_file_path = sys.argv[1]
-csv_output_path = sys.argv[2]
+# Parse command line arguments for bibliography
+bibliographies = ['beta', 'bitagap', 'biteca']
 
-# Read the data from the CSV file
-data = pd.read_csv(csv_file_path)
+parser = argparse.ArgumentParser()
+parser.add_argument('--bib', default='beta', choices=bibliographies, help='Specify a bibliography from the list.  Default is beta.')
+args = parser.parse_args()
 
-# Find the first column name from the CSV
-first_column_name = data.columns[0]
-# Print the output
-print("First column name:", first_column_name)
+bibliography = args.bib.upper()
 
-# Group the data by "MANID" column and export to csv
-grouped_data = data.groupby(first_column_name).first()
-grouped_data.to_csv(csv_output_path)
+# Directory path to search for files
+directory_path = f'../data/raw/{bibliography}/csvs/'
+
+# Find all files in the directory
+files = []
+for root, _, filenames in os.walk(directory_path):
+    for filename in filenames:
+        files.append(os.path.join(root, filename))
+
+# Print the list of files
+print("Files in directory:", files)
+
+# Create the first_row directory if it does not exist
+cwd = os.getcwd()
+updated_path = f'{cwd}/first_row/{bibliography}'
+Path(f'{updated_path}').mkdir(parents=True, exist_ok=True)
+
+for csv_file_path in files:
+    # Extract the string to the right of the first "-"
+    file_name = os.path.basename(csv_file_path)
+    file_name_parts = file_name.split("-")
+    file_name_parts = [part.strip() for part in file_name_parts]
+    extracted_string = file_name_parts[1]
+    print(f'Processing file: {csv_file_path}')
+    csv_output_path = f'{updated_path}/{bibliography}_{extracted_string}_first_row_{date}.csv'
+    # Read the data from the CSV file
+    data = pd.read_csv(csv_file_path)
+    # Find the first column name from the CSV
+    first_column_name = data.columns[0]
+    # Print the output
+    print("First column name:", first_column_name)
+    # Group the data by "MANID" column and export to csv
+    grouped_data = data.groupby(first_column_name).first()
+    grouped_data.to_csv(csv_output_path)
+
+print(f'Complete. See the updated files in the first_row/{bibliography} directory.')
