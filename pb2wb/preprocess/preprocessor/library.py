6f25522a5e956e847c7a6a591d8f261d40bf1195
Max Ziff <ziff@altiscale.com>
2024-09-24 17:05:24 -0700

    add full address

--- pb2wb/preprocess/preprocessor/library.py
+++ pb2wb/preprocess/preprocessor/library.py
@@ -11,6 +11,18 @@ class LibraryPreprocessor(GenericPreprocessor):
 
   TABLE = Table.LIBRARY
 
+  def format_library_address(self, row):
+    cnames = ['ADDRESS', 'PCODE', 'CITY', 'STATE', 'COUNTRY']
+    components = {n: row[n] for n in cnames}
+    if all(value is None or value == '' for value in components.values()):
+      result = ''
+    else:
+      components['STATE'] = f"({components['STATE']})" if components['STATE'] is not None and len(
+        components['STATE']) > 0 else components['STATE']
+      components = [components[n] for n in cnames if components[n] is not None and len(components[n]) > 0]
+      result = ' '.join(components)
+    return result
+
   def __init__(self, top_level_bib=None, qnumber_lookup_file=None) -> None:
     super().__init__(top_level_bib, qnumber_lookup_file)
 
@@ -26,6 +38,9 @@ class LibraryPreprocessor(GenericPreprocessor):
 
     # add new columns for the qnumbers using the lookup table if supplied
     df = self.add_qnumber_columns(df, LibraryPreprocessor.TABLE)
+
+    df['FULL_ADDRESS'] = df.apply(self.format_library_address, axis=1)
+    df = self.move_last_column_after(df, 'PCODE')
 
     self.write_result_csv(df, file)
     print(f'{datetime.now()} INFO: done')
