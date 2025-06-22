ceae31e93b9eae334d45f8ca14b00d24e889ea7c
Jason Hatfield <jhatfi@gmail.com>
2025-06-22 10:30:32 -0700

    update for spacing

--- pb2wb/preprocess/preprocessor/generic.py
+++ pb2wb/preprocess/preprocessor/generic.py
@@ -59,7 +59,7 @@ class GenericPreprocessor:
   def lookupDataclip(self, code, lang):
     lookupprefix = ''
     if self.top_level_bib.value == 'BETA':
-        lookupprefix = 'BETA '
+      lookupprefix = 'BETA '
     lookup_value = lookupprefix + code
     # print(f'{lookup_value = }')
     row = self.df_dataclip.loc[self.df_dataclip['code']==lookup_value]
